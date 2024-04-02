21568d6a932b94ecbde1c2e3b2c0dd5eee22e7c6
Guilherme-Leonardo <gui.la.berto25@gmail.com>
2024-04-02 16:29:32 -0300

    Envio dos codigos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PageHeaderComponent } from './componentes/page-header/page-header.component';
+import { PageMainComponent } from './componentes/page-main/page-main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageHeaderComponent, PageMainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
